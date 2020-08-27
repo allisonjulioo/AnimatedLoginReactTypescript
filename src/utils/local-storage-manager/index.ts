7e873c4726467bd461dd037c894f3fed250c216e
Allison Nunes <allison.julio@hotmail.com>
2020-08-27 12:51:31 -0300

    Fix bug localstorage

--- src/utils/local-storage-manager/index.ts
+++ src/utils/local-storage-manager/index.ts
@@ -1,11 +1,12 @@
-import { ContentHeader } from "../../models/interfaces/header";
+import { ContentHeader } from '../../models/interfaces/header';
 
 export class LocalStorageManager {
   public setItem(key: string, data: string) {
     localStorage.setItem(key, data);
   }
   public getItem(key: string): ContentHeader {
-    return JSON.parse(localStorage.getItem(key) || '');
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : {};
   }
   public removeItem(key: string) {
     localStorage.removeItem(key);
